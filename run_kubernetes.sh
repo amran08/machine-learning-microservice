9f5a8e1a3fcd6cf26f7742a47538ebff67dc0817
amranu.islam <amranul..islam@grameenphone.com>
2020-06-03 04:06:14 +0600

    port forwarding

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -16,7 +16,9 @@ kubectl create deployment mlcluster --image=$dockerpath
 kubectl get pods
 # Step 4:
 # Forward the container port to a host
-kubectl proxy
+#kubectl proxy
+
+kubectl port-forward deployment/mlcluster  8001:80
 
 export POD_NAME=$(kubectl get pods -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
 echo Name of the Pod: $POD_NAME
